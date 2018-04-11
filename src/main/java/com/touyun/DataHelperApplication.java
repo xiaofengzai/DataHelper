197cea3fe3bad1de9a34d1ae9e809cdcec2e8b06
root <feng.wen@sao.so>
2018-04-12 07:45:29 +0800

    fff

---
+++ src/main/java/com/touyun/DataHelperApplication.java
@@ -0,0 +1,14 @@
+package com.touyun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class DataHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataHelperApplication.class, args);
+    }
+}
