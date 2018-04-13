e99eb74e9f72814bfebcb85009834d492bd75920
wenfeng <feng.wen@sao.so>
2018-04-13 10:27:36 +0800

    配置多数据源

---
+++ src/main/java/com/touyun/config/JdbcTemplateProvider.java
@@ -0,0 +1,29 @@
+package com.touyun.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by wenfeng on 2018/4/13.
+ */
+@Service
+public class JdbcTemplateProvider {
+    @Autowired
+    @Qualifier("primaryJdbcTemplate")
+    protected JdbcTemplate primaryJdbcTemplate;
+    @Autowired
+    @Qualifier("secondaryJdbcTemplate")
+    protected JdbcTemplate secondaryJdbcTemplate;
+
+    public JdbcTemplate getJdbcTemplate(DatasourceTypeEnum datasourceTypeEnum){
+       switch (datasourceTypeEnum){
+           case Secondary:
+               return secondaryJdbcTemplate;
+               default:
+                   return  primaryJdbcTemplate;
+       }
+
+    }
+}
